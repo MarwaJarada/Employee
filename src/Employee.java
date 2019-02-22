0a5513412630aed5c2f3f4c4667b0b3e6371a071
Your-Full-Name <jaradamarwa@gmail.com>
2019-02-22 15:14:45 +0200

    preparing Employee class and basic attributes

--- src/Employee.java
+++ src/Employee.java
@@ -1,4 +1,46 @@
 public class Employee {
-    
+    private String name;
+    private String depatment;
+    private double salary;
+    public static int count;
+
+    public Employee(String name, String depatment, double salary) {
+        count++;
+        this.name = name;
+        this.depatment = depatment;
+        this.salary = salary;
+    }
+
+    public Employee() {
+        count++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepatment() {
+        return depatment;
+    }
+
+    public void setDepatment(String depatment) {
+        this.depatment = depatment;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public static int getCount(){
+        return count;
+    }
 
 }
